e203eceba1735baee2677994d158b0e10f5ba6cb
ZenekeZene <hectorvillarm@gmail.com>
2019-08-05 11:28:21 +0200

    Mobile first

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -5,15 +5,15 @@
 			<h1>Discover the newsletter which helps more
 				than 1000 designers,
 				creatives and researchers</h1>
-			<div class="subscribe">
-				<input type="email">
-				<button class="btn">Subscribe</button>
-			</div>
 			<p>Get the last curated articles by top designers,
 				resources and tools and learning materials to
 				achieve a successful career in design, we are
 				sure about it 😚
 			</p>
+			<div class="subscribe">
+				<input type="email">
+				<button class="btn">Subscribe</button>
+			</div>
 			<ul class="features">
 				<li>Latest relevant articles</li>
 				<li>Handpicked resources</li>
@@ -21,6 +21,9 @@
 				<li>Lots and lots of love 💜</li>
 			</ul>
 		</section>
+		<footer>
+			<p>Published by Juan Jesús Millo. Developed by Héctor Villar Mozo.</p>
+		</footer>
 	</article>
 </template>
 <script>
