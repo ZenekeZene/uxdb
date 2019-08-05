68c5d98b5d0ae99dc0e006a93c875d0f388037ba
ZenekeZene <hectorvillarm@gmail.com>
2019-08-05 23:12:59 +0200

    Transitions between pages

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
 		<router-link to="archive">Archive</router-link>
 		<router-link to="sponsorship">Sponsorship</router-link>
 	</nav>
-	<router-view/>
+	<transition name="fade" mode="out-in">
+		<router-view/>
+	</transition>
 	<footer class="footer" margin-top-2>
 		<p>Published by Juan Jesús Millo. Developed by Héctor Villar Mozo.
 			<a href="#">Follow us on Linkedin</a>.</p>
